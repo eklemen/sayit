a13a01d44e400264c041e929d821456f9b3af204
eklemen <ejklemen@gmail.com>
2023-09-26 19:58:08 -0400

    init

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { RecoilRoot } from 'recoil';
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
