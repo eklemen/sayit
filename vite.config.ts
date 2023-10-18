316a78feed89b91c30232239aec1f4f09145de2a
eklemen <ejklemen@gmail.com>
2023-10-17 20:01:54 -0400

    router set up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+      '@pages': path.resolve(__dirname, './src/Pages'),
+    },
+  },
   plugins: [react()],
 });
